f7e78e969c0a00b4947d05583ae244d59db8acdb
JerryWu <jwu@digitalairstrike.com>
2020-11-16 15:53:35 -0700

    added log message

--- dkcrawler.py
+++ dkcrawler.py
@@ -34,7 +34,8 @@ class DKCrawler:
         'apply-all': '[data-testid="apply-all-button"]',
         'popup-trigger': '[data-testid="download-table-popup-trigger-button"]',
         'download': '[data-testid="download-table-button"]',
-        'next-page': '[data-testid="btn-next-page"]'
+        'next-page': '[data-testid="btn-next-page"]',
+        'max-page': '[data-testid="per-page-selector-container"] > div:last-child > span'
     }
 
     def __init__(self, url, driver_path, download_dir):
@@ -80,6 +81,11 @@ class DKCrawler:
         apply_all.click()
         rand_delay(1, 3)
 
+    def _get_max_page(self):
+        max_page = self.browser.find_element_by_css_selector(self.selectors['max-page']).text
+        max_page = int(max_page.split('/')[-1])
+        return max_page
+
     def _click_download(self):
         popup_trigger = self.browser.find_element_by_css_selector(self.selectors['popup-trigger'])
         popup_trigger.click()
@@ -93,36 +99,47 @@ class DKCrawler:
         btn_next_page.click()
         rand_delay(5, 10)
 
-    def _rename_file(self, index):
+    def _rename_file(self, cur_page):
         try:
             downloaded_file = get_latest_file(self.download_dir)
-            renamed_file = os.path.join(self.download_dir, f'{self.product_category}_{index}.csv')
+            print(f'Downloaded file: "{downloaded_file}"')
+            renamed_file = os.path.join(self.download_dir, f'{self.product_category}_{cur_page}.csv')
             os.rename(downloaded_file, renamed_file)
+            print(f'Renamed file: "{downloaded_file}" -> "{renamed_file}"')
         except ValueError:
             pass
 
-    def merge_data(self):
+    def _merge_data(self):
         files = get_file_list(self.download_dir)
         combined_data = pd.concat([pd.read_csv(f) for f in files])
+        combined_data.drop_duplicates(inplace=True)
         combined_data.to_excel(f'{os.path.join(self.download_dir, self.product_category)}_all.xlsx',
                                index=False, encoding='utf-8-sig')
 
+    def _del_prev_files(self):
+        files = get_file_list(self.download_dir)
+        for f in files:
+            os.remove(f)
+
     def crawl(self):
+        self._del_prev_files()
         self.browser.get(self.url)
         self._set_page_size_100()
         self._select_in_stock()
+        max_page = self._get_max_page()
 
         try:
-            index = 1
+            cur_page = 1
             while True:
                 self._click_download()
-                self._rename_file(index)
-                index += 1
-                try:
+                self._rename_file(cur_page)
+                cur_page += 1
+
+                if cur_page <= max_page:
                     self._click_next_page()
-                except NoSuchElementException:
+                else:
                     break
-            self.merge_data()
+            self._merge_data()
         except NoSuchElementException:
             traceback.print_stack()
         finally:
@@ -133,7 +150,8 @@ def main():
     driver_path = 'geckodriver.exe'
     download_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
 
-    urls = ['https://www.digikey.com/en/products/filter/d-sub-cables/461',
+    urls = ['https://www.digikey.com/en/products/filter/d-sub-connectors/439',
+            'https://www.digikey.com/en/products/filter/d-sub-cables/461',
             'https://www.digikey.com/en/products/filter/usb-cables/455']
 
     for url in urls:
