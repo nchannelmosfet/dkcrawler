de944a725f2b8366fdf667bcd48b07a5fe8b333f
JerryWu <jwu@digitalairstrike.com>
2020-12-16 14:08:21 -0700

    updated install_pkg script

--- install_pkg.ps1
+++ install_pkg.ps1
@@ -1,3 +1,16 @@
+py -3 -m pip install virtualenv
+py -3 -m virtualenv venv
+
+# download and unzip Geckodriver for using selenium webdriver with Firefox
+$gecko_path = '.\src\geckodriver.exe'
+if (!(Test-Path -path $gecko_path)) {
+	$gecko_url = "https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-win64.zip"
+	$gecko_zip = '.\src\geckodriver.zip'
+	Invoke-WebRequest -Uri $gecko_url -OutFile $gecko_zip
+	Expand-Archive -Path $gecko_zip -DestinationPath '.\src'
+	Remove-Item $gecko_zip
+}
+
 .\venv\Scripts\python.exe -m pip install -r requirements.txt
 .\venv\Scripts\python.exe -m jupyter nbextension enable --py widgetsnbextension
 .\venv\Scripts\python.exe -m jupyter labextension install @jupyter-widgets/jupyterlab-manager
